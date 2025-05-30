cde616c2171d5a3d0e3e58099b490d74e70d5a57
1915950 <1915950@01-18130.pusd.dom>
2025-05-30 13:25:50 -0700

    Finally got the home screen to show up. Now working on adding hearts --> then game functionality

---
+++ GameScreen.java
@@ -0,0 +1,19 @@
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GameScreen extends HomeScreen {
+	
+
+	
+	GameScreen() {
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
--- HomeScreen.java
+++ HomeScreen.java
@@ -1,53 +1,76 @@
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+
 public class HomeScreen {
-	
-	private JFrame frame;
-	private JPanel panel;
-	private JLabel background;
-	private ImageIcon chipotleBackground;
-	
-	
-	
-	HomeScreen(){
-		panel = new JPanel();
-		frame = new JFrame("Chipotle Vault");
+
+    public JFrame frame;
+    private JPanel panel;
+    private JLabel background;
+    private ImageIcon chipotleBackground;
+    private boolean isPlaying;
+
+    HomeScreen() {
+        isPlaying = false;
+        panel = new JPanel();
+        frame = new JFrame("Chipotle Vault");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 750);
-        frame.setLayout(null);
+        frame.setSize(500, 780);
         frame.setLocationRelativeTo(null);
+
+        chipotleBackground = new ImageIcon("images/HomePage.jpg");
+        background = new JLabel(chipotleBackground);
+
+        panel.add(background);
+
        
-        
-		
-		chipotleBackground = new ImageIcon("H:\\git\\newChipotleGame\\images\\HomePage.jpg");
-		background = new JLabel(chipotleBackground);
-		
-		
-		panel.add(background);
-		
-		
-		
-		
-		
-		frame.add(panel);
-		frame.setVisible(true);
-		
-	}
-	
-	public void show() {
-		
-		
-		frame.revalidate();
-		frame.repaint();
-		
-		
-		
-	}
-	
-	
-	
-	
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+                System.out.println("X: " + x + " Y: " + y);
+                
+                if (x > 146 && x < 340 && y > 491 && y < 591 && !isPlaying) {
+                    JOptionPane.showMessageDialog(
+                        frame,
+                        "To win, you must guess the correct combination of ingredients for a Chipotle meal!\n" +
+                        "You only get 3 tries so make the most of it. Winners receive a BOGO chipotle code!"
+                    );
+                }
+                
+                if (x > 127 && x < 371 && y > 380 && y < 449 && !isPlaying) {
+                    // Start button logic here
+                	isPlaying = true;
+                	panel.remove(background);
+                	
+                	ImageIcon gameImage = new ImageIcon("images/GameScreen.png");
+                	
+                	background = new JLabel(gameImage);
+                	
+                	panel.add(background);
+                	
+                	
+                	frame.revalidate();
+                	frame.repaint();
+                	
+                	
+                }
+            }
+        });
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+
+    public void show() {
+        frame.revalidate();
+        frame.repaint();
+    }
 }

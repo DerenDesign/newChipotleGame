e3136026038b846710bcbe421dfed9788ea5f008
1962991 <1962991@01-18131.pusd.dom>
2025-06-03 14:05:19 -0700

    Implemented the X mark for incorrect guesses. and the White hearts for lives. Still some error with the correct answer as some correct ingredients are being marked incorrect and the correcty answer ch ecker is not working properly.

--- Random.java
+++ Random.java
@@ -1,16 +1,15 @@
 public class Random {
 
     private String[] correctIngredients;
-    private static final String[] ALL_INGREDIENTS = {
-        "No Rice", "Black Beans", "Pinto Beans", "Veggies", "Beef Barbocoa",
+    public static final String[] ALL_INGREDIENTS = {
+        "No Rice", "Black Beans", "Pinto Beans", "Veggies", "Beef Barbacoa",
         "Chicken", "Sofritas", "Carnitas", "Mild", "Spicy1", "Spicy2",
         "Corn", "Sour Cream", "Cheese", "Guac", "Lettuce"
     };
 
     public Random() {
-		int numIngredients = (int)(Math.random() * ALL_INGREDIENTS.length) + 1;
         correctIngredients = new String[3];
-        boolean[] used = new boolean[ALL_INGREDIENTS.length]; // Track used indices
+        boolean[] used = new boolean[ALL_INGREDIENTS.length];
         int selected = 0;
 
         while (selected < 3) {
@@ -22,7 +21,6 @@ public class Random {
             }
         }
 
-        // Debug output to verify selection
         System.out.println("Random ingredients: " + 
             correctIngredients[0] + ", " + 
             correctIngredients[1] + ", " + 
